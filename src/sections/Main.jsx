bea89195e4d6b6177438389dfa400d595b751871
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-02-28 14:54:44 +0200

    Update Main.jsx tablet design

--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -15,8 +15,8 @@ import linePatterns from "../assets/patterns/pattern-lines.svg";
 
 function Main() {
   return (
-    <main className="text-center absolute top-[670px] z-50 md:top-[900px] w-full xl:top-[780px]">
-      <div className="our-restaurant xl:flex xl:w-[80%] xl:mx-auto xl:gap-[6em] xl:items-center">
+    <main className="text-center absolute top-[670px] z-50 md:top-[900px] w-full xl:top-[90%]">
+      <div className="our-restaurant xl:flex xl:w-[1000px] xl:mx-auto xl:gap-[6em] xl:items-center 2xl:w-[80%]">
         <img
           src={enjoyablePlaceMobile}
           alt="green forest"
@@ -53,41 +53,47 @@ function Main() {
         </div>
       </div>
 
-      <div className="our-restaurant mx-auto mt-10 relative w-[100%] ">
-        <img
-          src={locallySourcedMobile}
-          alt="a man is cooking"
-          className="mx-auto md:hidden main-section-img"
-        />
-        <img
-          src={locallySourcedTablet}
-          alt="a man is cooking"
-          className="hidden md:block xl:hidden main-section-img mx-auto main-section-img"
-        />
-        <img
-          src={locallySourcedDesktop}
-          alt="a man is cooking"
-          className="mx-auto hidden xl:block main-section-img"
-        />
-        <img
-          src={linePatterns}
-          className="hidden md:block absolute top-[230px] left-[50%] transform translate-x-[115%]"
-        />
-        <img
-          src={topLeftPattern}
-          className="hidden md:block absolute h-[310px] top-[400px] left-[50%] w-[50%] z-[-2222]"
-        />
-        <div>
-          <img src={patternImg} alt="pattern img" className="mx-auto mt-12" />
-          <h2 className="text-[#242B37] font-bold text-[2em] pt-8 leading-10 tracking-tight md:text-[3em] md:leading-[1em]">
-            The most locally <br />
-            sourced food
-          </h2>
-          <p className="text-[#242B37] pt-5 w-[80%] mx-auto md:text-[1.2em] md:w-[60%]">
-            All our ingredients come directly from our farm or local fishery. So
-            you can be sure that you’re eating the freshest, most sustainable
-            food.
-          </p>
+      <div className="xl:w-[100%] xl:mx-auto">
+        <div className="our-restaurant mx-auto mt-10 relative w-[100%] xl:flex xl:items-center xl:w-[1000px] 2xl:w-[80%] xl:mt-[7em]">
+          <img
+            src={locallySourcedMobile}
+            alt="a man is cooking"
+            className="mx-auto md:hidden main-section-img"
+          />
+          <img
+            src={locallySourcedTablet}
+            alt="a man is cooking"
+            className="hidden md:block xl:hidden main-section-img mx-auto main-section-img"
+          />
+          <img
+            src={locallySourcedDesktop}
+            alt="a man is cooking"
+            className="mx-auto hidden xl:block main-section-img order-2"
+          />
+          <img
+            src={linePatterns}
+            className="hidden md:block absolute top-[230px] left-[50%] transform translate-x-[115%] xl:top-[300px] xl:translate-x-[290%] 2xl:translate-x-[350%]"
+          />
+          <img
+            src={topLeftPattern}
+            className="hidden md:block absolute h-[310px] top-[400px] left-[50%] w-[50%] z-[-2222] "
+          />
+          <div className="xl:text-left">
+            <img
+              src={patternImg}
+              alt="pattern img"
+              className="mx-auto mt-12 xl:m-0"
+            />
+            <h2 className="text-[#242B37] font-bold text-[2em] pt-8 leading-10 tracking-tight md:text-[3em] md:leading-[1em]">
+              The most locally <br />
+              sourced food
+            </h2>
+            <p className="text-[#242B37] pt-5 w-[80%] mx-auto md:text-[1.2em] md:w-[60%] xl:m-0 xl:text-[1.4em]">
+              All our ingredients come directly from our farm or local fishery.
+              So you can be sure that you’re eating the freshest, most
+              sustainable food.
+            </p>
+          </div>
         </div>
       </div>
     </main>
