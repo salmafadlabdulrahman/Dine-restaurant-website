d1071788ccab7c13430c4eaf870c3bc9b7d4e247
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-02-29 16:52:09 +0200

    update menu section tablet design

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,16 +1,23 @@
-function MenuCard({name, description, mobileImg, desktopImg, index}) {
+function MenuCard({ name, description, mobileImg, desktopImg, index }) {
   return (
-    <div className="pt-[4em] px-3">
-      <img src={mobileImg} className="mx-auto md:hidden" />
-      <img src={desktopImg} className="hidden md:block" />
-      <div>
-        <h4 className="font-extrabold mt-5 text-[1.2em]">
-          {name}
-        </h4>
-        <p className="w-[80%] mx-auto pt-3 pb-[2em]">
-          {description}
-        </p>
-        <p className={`w-[95%] ${index !== "" && `border-[0.3px] border-[#333333]`} mx-auto `}></p>
+    <div className="pt-[4em] px-3  food-card border border-blue-900 ">
+      <div className="recipe-info md:flex md:items-center justify-center  md:w-[70%] md:mx-auto">
+        <img src={mobileImg} className="mx-auto md:hidden" />
+        <img
+          src={desktopImg}
+          className="hidden md:block border border-violet-800"
+        />
+        <div>
+          <h4 className="font-extrabold mt-5 text-[1.2em]">{name}</h4>
+          <p className="w-[75%] mx-auto pt-3 pb-[2em] md:w-[50%]">
+            {description}
+          </p>
+          <p
+            className={`w-[95%] ${
+              index !== "" && `border-[0.3px] border-[#333333]`
+            } mx-auto md:w-[full]`}
+          ></p>
+        </div>
       </div>
     </div>
   );
--- src/sections/MenuHighlights.jsx
+++ src/sections/MenuHighlights.jsx
@@ -7,10 +7,10 @@ function MenuHighlights() {
     <div className="bg-[#111111] text-white pt-[4em] px-3">
       <div className="menu-hightlight-container">
         <img src={patternImg} className="mx-auto" />
-        <h3 className="mt-6 text-[1.8em] font-extrabold">
+        <h3 className="mt-6 text-[1.8em] font-extrabold md:text-[2.9em] md:font-bold md:leading-[1em]">
           A few highlights from <br /> our menu
         </h3>
-        <p className="mt-3 w-[85%] mx-auto">
+        <p className="mt-3 w-[85%] mx-auto md:w-[55%] text-[1.2em]">
           We cater for all dietary requirements, but here’s a glimpse at some of
           our diner’s favorites. Our menu is revamped every season.
         </p>
@@ -19,13 +19,13 @@ function MenuHighlights() {
       <div className="menu-cards-wrapper">
         {menu.map((item, index) => (
           <MenuCard
-          key={index}
-          name={item.name}
-          description={item.description}
-          mobileImg={item.mobileImg}
-          desktopImg={item.desktopImg}
-          index={index !== menu.length - 1 ? index : ""}
-        />
+            key={index}
+            name={item.name}
+            description={item.description}
+            mobileImg={item.mobileImg}
+            desktopImg={item.desktopImg}
+            index={index !== menu.length - 1 ? index : ""}
+          />
         ))}
       </div>
     </div>
