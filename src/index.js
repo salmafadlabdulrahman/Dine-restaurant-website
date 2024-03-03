6b7773344b2a594091cc6975e16a2c4357b7be63
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-03-03 21:23:12 +0200

    Add family gathering content

--- src/index.js
+++ src/index.js
@@ -19,6 +19,18 @@ import beefTabletImg from "./assets/homepage/beef-desktop-tablet.jpg";
 import chocolateMobileImg from "./assets/homepage/chocolate-mobile.jpg";
 import chocolateTabletImg from "./assets/homepage/chocolate-desktop-tablet.jpg";
 
+import familyGatheringMobile from "./assets/homepage/family-gathering-mobile.jpg";
+import familyGatheringTablet from "./assets/homepage/family-gathering-tablet.jpg";
+import familyGatheringDesktop from "./assets/homepage/family-gathering-desktop.jpg";
+
+import specialEventsMobile from "./assets/homepage/special-events-mobile.jpg";
+import specialEventsTablet from "./assets/homepage/special-events-tablet.jpg";
+import specialEventsDesktop from "./assets/homepage/special-events-desktop.jpg";
+
+import socialEventsMobile from "./assets/homepage/social-events-mobile.jpg";
+import socialEventsTablet from "./assets/homepage/social-events-tablet.jpg";
+import socialEventsDesktop from "./assets/homepage/social-events-desktop.jpg";
+
 const menu = [
   {
     name: "Seared Salmon Fillet",
@@ -41,7 +53,34 @@ const menu = [
     mobileImg: chocolateMobileImg,
     desktopImg: chocolateTabletImg,
   },
-]
+];
+
+const events = [
+  {
+    name: "Family Gathering",
+    description:
+      "We love catering for entire families. So please bring everyone along for a special meal with your loved ones. We’ll provide a memorable experience for all.",
+    mobileImg: familyGatheringMobile,
+    tabletImg: familyGatheringTablet,
+    desktopImg: familyGatheringDesktop,
+  },
+  {
+    name: "Special Events",
+    description:
+      "Whether it’s a romantic dinner or special date you’re celebrating with others we’ll look after you. We’ll be sure to mark your special date with an unforgettable meal.",
+    mobileImg: specialEventsMobile,
+    tabletImg: specialEventsTablet,
+    desktopImg: specialEventsDesktop,
+  },
+  {
+    name: "Social Events",
+    description:
+      "Are you looking to have a larger social event? No problem! We’re more than happy to cater for big parties. We’ll work with you to make your event a hit with everyone.",
+      mobileImg: socialEventsMobile,
+      tabletImg: socialEventsTablet,
+      desktopImg: socialEventsDesktop,
+  },
+];
 
 export {
   headerBgMobile,
@@ -53,5 +92,5 @@ export {
   locallySourcedMobile,
   locallySourcedTablet,
   locallySourcedDesktop,
-  menu
+  menu,
 };
